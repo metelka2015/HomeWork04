3638dda5cba18e23b56a5b2811977241270a01b4
Elena <metelka2015@gmail.com>
2022-09-05 00:22:00 +0500

    Задача 29

---
+++ HW04_Ex29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void CreateArray(int[] array)
+{
+    var random = new Random();
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int j = 0; j < count; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+}
+int[] array = new int[8];
+CreateArray(array);
+PrintArray(array);
